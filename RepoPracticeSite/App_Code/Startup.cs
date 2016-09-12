b86a1dbd7ebfd9aad5b0ab90103d3f50ff398292
BLeenheer <bleenheer@gmail.com>
2016-09-12 16:18:00 -0600

    Repository Practice Project - 1st Version
    After creating the initial project, this is the default website with the
    default pages and files.

---
+++ RepoPracticeSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepoPracticeSite.Startup))]
+namespace RepoPracticeSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
